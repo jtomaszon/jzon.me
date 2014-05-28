36c45226fecc1a2a1e37494656b770bc302f1ce8
Javier Tomas Zon <javier.zon@fluig.com>
2014-05-28 17:09:24 -0300

    Adding compression headers on response

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compress = require('compression');
 
 var routes = require('./routes/index');
 var app = express();
@@ -18,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compress());
 app.locals.pretty = true;
 
 
