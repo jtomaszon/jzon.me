04f258adeb0a2fed396509c1c5529e1351cf4a53
Javier Tomas Zon <javier.zon@fluig.com>
2014-05-28 17:13:55 -0300

    Adding compression headers on response

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compress());
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(compress());
 app.locals.pretty = true;
 
 
